bb5d1e39201e39fae626e7d10f6dafc08bec154c
DogsAreHumansToo <marc_laban@hotmail.com>
2022-11-16 04:24:00 +1300

    Commenting - ML

--- Habitable/Assets/TypeBClass.cs
+++ Habitable/Assets/TypeBClass.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//class that calls from the parent interface that defines the
+//resourcetype function. Debug log states what type of resource this is
 public class TypeBClass : ResourceType
 {
     public void Resourcetype()
--- Habitable/Assets/TypeCResource.cs
+++ Habitable/Assets/TypeCResource.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//child of the resource class that is the acutal resource type
+//initializes the different type then sets that as its type through
+//the function from the parent class
 public class TypeCResource : Resource
 {
     void Start()
