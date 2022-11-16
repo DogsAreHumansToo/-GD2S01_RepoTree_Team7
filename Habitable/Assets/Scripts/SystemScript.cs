93172d8c9f573f8c5b9d3f3cc2bd7001c602de9a
Seth Peters <sethpetersnz@gmail.com>
2022-11-16 17:23:02 +1300

    Added Timer
    Added timer that counts up in seconds and keeps track of how many days have passed

--- Habitable/Assets/Scripts/SystemScript.cs
+++ Habitable/Assets/Scripts/SystemScript.cs
@@ -1,19 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class SystemScript : MonoBehaviour
 {
     [SerializeField] private GameObject winUI;
     [SerializeField] private GameObject loseUI;
+    
+    // Timer Variables
+    static public float timeElapsed = 0;
+    static public float timeLeft = 24000;
+    static public float dayTime = 0;
+    static public int currentDay = 0;
 
-    // update
-    // {
-    //      if(lose condition == true)
-    //      enable game over UI
-    //      else if (win condition == true)
-    //      enable win UI
-    // }
+    public bool timerIsRunning = false;
+    // /Timer Variables
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+    void Update()
+    {
+        // Timer
+        if (timerIsRunning)
+        {
+            if (timeElapsed < 24000)
+            {
+                timeElapsed += Time.deltaTime;
+                timeLeft -= Time.deltaTime;
+
+                // Days
+                if (dayTime < 24)
+                {
+                    dayTime += Time.deltaTime;
+                }
+                else if (dayTime == 24)
+                {
+                    currentDay += 1;
+                    Debug.Log("Days: " + currentDay);
+                    dayTime = 0;
+                }
+
+            }
+            else
+            {
+                Debug.Log("Millenium Complete");
+                Debug.Log("Days: " + currentDay);
+                timeElapsed = 24000;
+                timerIsRunning = false;
+            }
+        }
+        // /Timer
+    }
+    
+    //update
+    //{
+    // 
+    //    if(lose condition == true)
+    //    enable game over UI
+    //    else if (win condition == true)
+    //    enable win UI
+    //}
 }
 //
