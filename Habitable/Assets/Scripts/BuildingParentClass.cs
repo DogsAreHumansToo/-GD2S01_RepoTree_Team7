f9725e2de586a51591f29dbf3d1ad562c649ea0f
DogsAreHumansToo <marc_laban@hotmail.com>
2022-11-16 14:31:08 +1300

    File Management and starting the building class with interface

---
+++ Habitable/Assets/Scripts/BuildingParentClass.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BuildingParentClass : MonoBehaviour
+{
+    //Initializing the resource type interface
+    BuildingType buildingType;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //calling the BuildingFunction function via the interface
+        buildingType.BuildingFunction();
+    }
+    //function thats sets the resource type
+    public void setBuildingType(BuildingType _newBuildingtype)
+    {
+        buildingType = _newBuildingtype;
+    }
+    void produceGoods()
+    {
+
+    }
+
+
+}
